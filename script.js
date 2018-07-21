cd905c136230cffbbad19375fc53dc5d34549f2a
gsevtlana <gsevtlana@gmail.com>
2018-07-20 21:11:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toInput(id) {
     let value = document.getElementById(id).value;
     document.getElementById('result').value += value;
 }
+
+function subtract(a, b){
+    console.log('subtract')
+    return a-b;
+}
